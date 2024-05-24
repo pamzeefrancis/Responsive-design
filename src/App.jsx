cbb8dbf4448044ab2ebfc15d39da85f6044a46a5
Pamelazee <144475369+Pamelazee@users.noreply.github.com>
2024-05-24 13:20:38 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="firstBox">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="secondBox"></div>
+        <div className="thirdBox">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="fourthBox">
+          <div></div>
+          <div></div>
+          <div className="remove"></div>
+          <div className="remove"></div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
